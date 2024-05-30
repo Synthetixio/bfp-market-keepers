699a59cfa319ec8987ccbc70a063acac4f430f54
David Vuong <david.vuong256@gmail.com>
2024-05-30 23:34:14 +1000

    chore languageOptions globals.node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 
 export default [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
