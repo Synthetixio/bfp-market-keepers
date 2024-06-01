b342c7fb260feabb0d742f68a98d5efe58485fb1
David Vuong <david.vuong256@gmail.com>
2024-06-01 21:36:20 +1000

    feat: use pino logs

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
 require('dotenv').config();
 
-import { erc20Abi, formatUnits, fromHex, getContract } from 'viem';
+import { erc20Abi, fromHex, getContract } from 'viem';
 import { sepolia } from 'viem/chains';
 import { getLogger } from './logger';
 import { getConfig } from './config';
 import { getBfpMarketProxy } from './contracts';
 import { MIN_MARGIN_USD } from './constants';
+import { Wei } from './wei';
 
 const config = getConfig();
 const logger = getLogger('main');
 
-export type Ctx = Awaited<ReturnType<typeof getBfpMarketProxy>>;
+type AwaitedOneLevel<T> = T extends Promise<infer U> ? U : T;
 
-const getAccountId = async ({ BfpMarketProxy, client, account }: Ctx) => {
+export type Ctx = AwaitedOneLevel<ReturnType<typeof getBfpMarketProxy>>;
+
+const getOrCreateAccount = async ({ BfpMarketProxy, client, account }: Ctx) => {
   const accountNftBalanceOf = await BfpMarketProxy.AccountTokenModule.read.balanceOf([
     account.address,
   ]);
   if (accountNftBalanceOf === 0n) {
-    logger.info('BFP NFT account not found, creating...');
+    logger.info('No account found. Creating new BFP NFT account');
     const hash = await BfpMarketProxy.AccountModule.write.createAccount();
     await client.waitForTransactionReceipt({ hash });
   }
@@ -29,48 +32,58 @@ const depositMargin = async (
   marketId: bigint,
   { BfpMarketProxy, client, wallet, account }: Ctx
 ) => {
-  const marginDigest = await BfpMarketProxy.MarginModule.read.getMarginDigest([
+  const { marginUsd } = await BfpMarketProxy.MarginModule.read.getMarginDigest([
     accountId,
     marketId,
   ]);
-  const marginUsd = formatUnits(marginDigest.marginUsd, 18);
-  if (marginDigest.marginUsd <= MIN_MARGIN_USD) {
-    logger.info(`Margin of $${marginUsd} below minimum $${formatUnits(MIN_MARGIN_USD, 18)}`);
-    logger.info('Depositing margin...');
 
-    const requiredMarginUsdDelta = MIN_MARGIN_USD - marginDigest.marginUsd;
-    logger.info(`Attempting to deposit USD delta $${formatUnits(requiredMarginUsdDelta, 18)}`);
+  if (marginUsd <= MIN_MARGIN_USD) {
+    logger.info(`Margin $${Wei.fmt(marginUsd)} below min $${Wei.fmt(MIN_MARGIN_USD)}`);
 
-    const accountDigest = await BfpMarketProxy.PerpAccountModule.read.getAccountDigest([
+    const marginUsdDelta = Wei.mul(MIN_MARGIN_USD - marginUsd, Wei.toWei(1.05));
+    const remMarginUsd = marginUsdDelta;
+    const cumMarginUsd = 0n;
+
+    logger.info(`Attempting to deposit $${Wei.fmt(marginUsdDelta)}`);
+
+    const { depositedCollaterals } = await BfpMarketProxy.PerpAccountModule.read.getAccountDigest([
       accountId,
       marketId,
     ]);
-    const cumMarginUsd = marginDigest.marginUsd;
 
-    for (const deposited of accountDigest.depositedCollaterals) {
+    for (const { collateralAddress, oraclePrice } of depositedCollaterals) {
       const ERC20Contract = getContract({
-        address: deposited.collateralAddress,
+        address: collateralAddress,
         abi: erc20Abi,
         client: { public: client, wallet },
       });
-      const [name, balanceOf] = await Promise.all([
-        ERC20Contract.read.name(),
+      const [symbol, balanceOf] = await Promise.all([
+        ERC20Contract.read.symbol(),
         ERC20Contract.read.balanceOf([account.address]),
       ]);
 
       if (balanceOf === 0n) {
-        logger.info(`Zero balance found '${name}'`);
+        logger.info(`ERC20(${symbol}).balanceOf=0`);
         continue;
       }
 
-      logger.info(`Depositing '${name}' to margin (${formatUnits(balanceOf, 18)} found)`);
+      const p = Wei.div(remMarginUsd, Wei.mul(balanceOf, oraclePrice));
+      const size = Wei.mul(p, balanceOf);
+      const depositUsd = Wei.fmt(Wei.mul(size, oraclePrice));
+
+      logger.info(`ERC20(${symbol}).balanceOf=${Wei.fmt(balanceOf)} ($${Wei.fmt(oraclePrice)})`);
+      logger.info(
+        `ERC20(${symbol}).transfer(trader, BfpMarketProxy, ${Wei.fmt(size)}) $${depositUsd}`
+      );
+
+      // BfpMarketProxy.MarginModule.write.modifyCollateral([accountId, marketId]);
     }
 
     if (cumMarginUsd === 0n) {
-      throw new Error('Unable to proceed insufficient margin');
+      throw new Error('Unable to proceed due to insufficient margin');
     }
   } else {
-    logger.info(`Proceeding with sufficient margin: $${marginUsd} `);
+    logger.info(`Proceeding with sufficient margin: ${Wei.fmt(marginUsd)}`);
   }
 };
 
@@ -78,7 +91,7 @@ const main = async () => {
   try {
     const ctx = await getBfpMarketProxy(sepolia, config.privateKey as `0x${string}`);
     const { BfpMarketProxy, account } = ctx;
-    logger.info(`Keeper address: ${account.address}`);
+    logger.info(`Trader address: ${account.address}`);
 
     const bfpActiveMarketIds =
       await BfpMarketProxy.PerpMarketFactoryModule.read.getActiveMarketIds();
@@ -88,12 +101,12 @@ const main = async () => {
     logger.info(`Active BFP markets found: [${bfpActiveMarketIds.join(',')}]`);
 
     const marketId = bfpActiveMarketIds[0];
-    const marketDigest = await BfpMarketProxy.PerpMarketFactoryModule.read.getMarketDigest([
+    const { name: marketName } = await BfpMarketProxy.PerpMarketFactoryModule.read.getMarketDigest([
       marketId,
     ]);
-    logger.info(`Using marketId: ${marketId} (${fromHex(marketDigest.name, 'string')})`);
+    logger.info(`Using marketId: ${marketId} (${fromHex(marketName, 'string')})`);
 
-    const accountId = await getAccountId(ctx);
+    const accountId = await getOrCreateAccount(ctx);
     logger.info(`Using accountId: ${accountId}`);
 
     await depositMargin(accountId, marketId, ctx);
--- src/logger.ts
+++ src/logger.ts
@@ -1,18 +1,16 @@
-import { map } from 'lodash';
-import winston from 'winston';
+import pino from 'pino';
+import { getConfig } from './config';
 
-export const getLogger = (label: string) =>
-  winston.createLogger({
-    level: process.env.LOG_LEVEL ?? 'info',
-    format: winston.format.combine(
-      winston.format.label({ label }),
-      winston.format.timestamp(),
-      winston.format.printf(({ timestamp, level, label, component, message, args }) => {
-        const argsMessage = map(args, (value, key) => `${key}=${value}`).join(' ');
-        return [timestamp, level, label, component, message, argsMessage]
-          .filter((x) => !!x)
-          .join(' ');
-      })
-    ),
-    transports: [new winston.transports.Console()],
+const config = getConfig();
+
+export const getLogger = (name: string) =>
+  pino({
+    name,
+    level: config.logLevel,
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+      },
+    },
   });
